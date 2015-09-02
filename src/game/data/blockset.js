3afdc8059b7a76413556114e2180e20d679ad5c1
j-som <308401057@qq.com>
2015-09-02 16:49:22 +0800

    improve blockset

--- src/game/data/blockset.js
+++ src/game/data/blockset.js
@@ -1,10 +1,14 @@
 var blockset = cc.Class.extend({
 	blocks:[],
+	width:0,
+	height:0,
 	ctor:function(row, column){
 		var count = row * column;
 		for (var i = 0; i < count; ++i) {
 			this.blocks[i] = 0;
 		};
+		this.width = column;
+		this.height = row;
 	},
 	merge:function (block)
 	{
@@ -13,7 +17,27 @@ var blockset = cc.Class.extend({
 		var c = block.column;
 		var r = block.row;
 		var data = block.blockData;
-		
+		var index = 0;
+		var indexOfNew = new Array();
+		for (var i = 0; i < r; ++i)
+		{
+			for (var j = 0; j < c; ++j)
+			{
+				if (data[i][j])
+				{
+					index = y * this.width + x;
+					indexOfNew.push(index);
+					this.blocks[index] = data[i][j];
+				}
+			}
+		}
+		//todo 根据indexOfNew 消去相应的块
+		_disposeBlocks(indexOfNew);
+	}
+
+	_disposeBlocks:function(indexs)
+	{
+
 	}
 	
 });
